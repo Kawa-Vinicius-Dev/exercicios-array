0fca64b5848f652d4424c341fc6824350b804c96
Kawa Vinicius <kawavinicioss@gmail.com>
2026-02-21 16:45:20 -0300

    feat: adiciona Exercicio9 - soma os digitos dos elementos de um array

---
+++ src/ExercicioArray/Exericicio9.java
@@ -0,0 +1,25 @@
+package ExercicioArray;
+
+/*
+* Write a Java program to find the sum of digits of each element in an array.
+*/
+
+public class Exericicio9 {
+    public static void main(String[] args) {
+
+        int[] numero = {111, 222, 333, 444, 555, 666, 777, 888, 999};
+
+        for (int i = 0; i < numero.length; i++) {
+
+            int numeroAtual = numero[i];
+            int soma = 0;
+
+            while (numeroAtual > 0){
+                int ultimoDigito = numeroAtual % 10;
+                soma += ultimoDigito;
+                numeroAtual /= 10;
+            }
+            System.out.println("A soma dos números: " + numero[i] + " é igual a: " + soma );
+        }
+    }
+}
